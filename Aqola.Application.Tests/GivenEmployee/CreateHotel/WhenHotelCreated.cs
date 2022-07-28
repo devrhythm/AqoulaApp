aef971b64668000e4d5d80b77b6f3056484bcb59
SUMETH K <sumeth_dev@live.com>
2022-07-28 21:37:44 +0700

    Add test case when hotel created by employee

---
+++ Aqola.Application.Tests/GivenEmployee/CreateHotel/WhenHotelCreated.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aqola.Application.Tests.GivenEmployee.CreateHotel
+{
+    public class WhenHotelCreated
+    {
+        [Fact]
+        public void ThenFloorShouldBeCreated()
+        {
+
+        }
+
+        [Fact]
+        public void ThenRoomPerFloorShouldBeCreated()
+        {
+
+        }
+
+        [Fact]
+        public void ThenAvailableRoomsShouldEqualsRoomCreated()
+        {
+
+        }
+
+        [Fact]
+        public void ThenKeycardShouldBeCreated()
+        {
+
+        }
+
+        [Fact]
+        public void ThenHotelCreatedResultShoudBeCorrect()
+        {
+
+        }
+    }
+}
