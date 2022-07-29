02c8813477471a2ae58cd0dbc9a5c2a0d122a3d2
SUMETH K <sumeth_dev@live.com>
2022-07-29 11:08:43 +0700

    Implement CreateHotelCommand

--- Aqola.Application/Commands/CreateHotelCommand.cs
+++ Aqola.Application/Commands/CreateHotelCommand.cs
@@ -1,4 +1,5 @@
-﻿using Aqola.Domain.Services;
+﻿using Aqola.Domain.Models;
+using Aqola.Domain.Services;
 
 namespace Aqola.Application.Services.Commands
 {
@@ -10,7 +11,15 @@ namespace Aqola.Application.Services.Commands
         }
         public string Execute(params object?[] options)
         {
-            throw new NotImplementedException();
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            int floor = options[0].ToInt();
+            int roomPerFloor = options[1].ToInt();
+            HotelCreatedResult result = _hotelService.CreateHotel(floor, roomPerFloor);
+            return result.Message;
         }
 
         public bool IsHandle(string command)
